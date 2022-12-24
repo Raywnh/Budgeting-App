7a0d3e7185bffe34739d0f829d15e377149ecc7f
Ray Ho <108425957+Raywnh@users.noreply.github.com>
2022-12-24 01:37:54 -0800

    created header bar with dropdown menu

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import MenuBar from './components/MenuBar';
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <MenuBar/>
+    )
+  }
+}
+ 
+export default App;
