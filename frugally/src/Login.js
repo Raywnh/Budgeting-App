62ab7fea3e1b62a3a630af4f4a0a284fb6139341
Ray Ho <108425957+Raywnh@users.noreply.github.com>
2023-01-01 00:38:15 -0800

    login page frontend done

--- frugally/src/Login.js
+++ frugally/src/Login.js
@@ -1,7 +1,18 @@
-import React from 'react'
+export default function login(props) {
 
-export default function login() {
   return (
-    <div className="login">login</div>
+    <div className="login">
+      <form>
+        <div class='user-div'>
+        <h2>Username: </h2>
+          <input/>
+        </div>
+        <div class='pass-div'>
+          <h2>Password: </h2> 
+          <input/>
+        </div>
+        <button>Enter</button>
+        </form>
+    </div>
   )
 }
